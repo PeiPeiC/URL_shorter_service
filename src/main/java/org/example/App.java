191dcbd90230a0f70c2ec2d42390e5cb3c720dda
PeiPeiC <l86426@gmail.com>
2023-04-08 00:37:09 +0800

    updated

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,29 @@
 package org.example;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/health", new HealthHandler());
+        server.setExecutor(null);
+        server.start();
+
+    }
+}
+class HealthHandler implements HttpHandler {
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "{\"status\":\"OK\"}";
+        exchange.sendResponseHeaders(200, response.length());
+        exchange.getResponseBody().write(response.getBytes());
+        exchange.getResponseBody().close();
     }
 }
