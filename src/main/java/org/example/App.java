ccd18eea2b25890ab5b22f5d83adc144b7d3ab09
PeiPeiC <l86426@gmail.com>
2023-04-10 00:32:41 +0800

    updated

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,7 +5,8 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-
+import org.example.handler.HelloHandler;
+import org.example.handler.HealthHandler;
 /**
  * Hello world!
  *
@@ -20,20 +21,5 @@ public class App {
         System.out.println("Server start");
     }
 }
-class HealthHandler implements HttpHandler {
-    public void handle(HttpExchange exchange) throws IOException {
-        String response = "{\"status\":\"OK\"}"; //the "response" string is created as a JSON message with a "status" field set to "OK" because the HTTP response is intended to be consumed by a client application, and JSON is a commonly used data format for transmitting data between web applications and client applications.
-        exchange.sendResponseHeaders(200, response.length()); //sends an HTTP response header with a status code of 200 (OK) and a content length equal to the length of the "response" string.
-        exchange.getResponseBody().write(response.getBytes()); //write() method expects an array of bytes as its argument, not a string. By calling response.getBytes(), we are converting the string into an array of bytes, which can then be written to the response body using the write() method.
-        exchange.getResponseBody().close();
-    }
-}
 
-class HelloHandler implements HttpHandler {
-    public void handle(HttpExchange exchange) throws IOException {
-        String response = "world";
-        exchange.sendResponseHeaders(200, response.length()); //sends an HTTP response header with a status code of 200 (OK) and a content length equal to the length of the "response" string.
-        exchange.getResponseBody().write(response.getBytes()); //write() method expects an array of bytes as its argument, not a string. By calling response.getBytes(), we are converting the string into an array of bytes, which can then be written to the response body using the write() method.
-        exchange.getResponseBody().close();
-        }
-}
+
---
+++ src/main/java/org/example/handler/HelloHandler.java
@@ -0,0 +1,15 @@
+package org.example.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class HelloHandler implements HttpHandler {
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "world";
+        exchange.sendResponseHeaders(200, response.length()); //sends an HTTP response header with a status code of 200 (OK) and a content length equal to the length of the "response" string.
+        exchange.getResponseBody().write(response.getBytes()); //write() method expects an array of bytes as its argument, not a string. By calling response.getBytes(), we are converting the string into an array of bytes, which can then be written to the response body using the write() method.
+        exchange.getResponseBody().close();
+    }
+}
