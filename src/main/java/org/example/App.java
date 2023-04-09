3b3fb368b8cc47c3fd3440bc95453ea88dfc8fba
Boris Chen <borischen0203@gmail.com>
2023-04-09 22:02:10 +0100

    Tide format

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,20 +1,27 @@
 package org.example;
-import java.io.IOException;
-import java.net.InetSocketAddress;
 
 import com.sun.net.httpserver.HttpServer;
-import org.example.handler.HelloHandler;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import org.example.handler.HealthHandler;
+import org.example.handler.HelloHandler;
+
 /**
  * Hello world!
- *
  */
 public class App {
+
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/health", new HealthHandler());//A context is used to define a specific part of a web application that can be accessed via a specific URI path.
-        server.createContext("/hello",new HelloHandler());
-        server.setExecutor(null); //Setting the executor to null effectively disables any additional thread management and uses the default thread pool provided by the HttpServer
+        /**
+         * A context is used to define a specific part of a web application that can be accessed via a specific URI path.
+         */
+        server.createContext("/health", new HealthHandler());
+        server.createContext("/hello", new HelloHandler());
+        /**
+         * Setting the executor to null effectively disables any additional thread management and uses the default thread pool provided by the HttpServer
+         */
+        server.setExecutor(null);
         server.start();
         System.out.println("Server start");
     }
